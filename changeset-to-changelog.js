eec18b4ec361bebea15f7c477aaeef2298efe706
Rich Gwozdz <rgwozdz@esri.com>
2023-09-01 12:37:16 -0700

    chore: modify-fork after reset

--- changeset-to-changelog.js
+++
@@ -1,7 +0,0 @@
-const { default: { getDependencyReleaseLine } } = require('@changesets/changelog-git');
-
-const getReleaseLine = async (changeset) => {
-  return changeset.summary;
-};
-
-module.exports = { getDependencyReleaseLine, getReleaseLine };
